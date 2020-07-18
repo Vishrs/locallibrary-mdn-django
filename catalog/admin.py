9d780e1cbaa4d7319276c439d1bfb092e8b5eda4
pintu <pintu@localhost.localdomain>
2020-07-18 17:45:19 -0400

    Added detail views to some models. Added inline viewing. AdminInstance intro

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
-
+from .models import Author, Book, BookInstance, Genre, Language
 # Register your models here.
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+#admin.site.register(BookInstance)
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# Inline for BookInstance for a Book model
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+# @admin is the same as admin.site.register(Book, BookAdmin)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        ('Details', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
--- catalog/models.py
+++ catalog/models.py
@@ -22,13 +22,20 @@ class Book(models.Model):
     author  = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-    isbn    = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn    = models.CharField('ISBN', max_length=13, 
+                                help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre   = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
     language = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
 
     def __str__(self):
         """String for representing the Model object."""
